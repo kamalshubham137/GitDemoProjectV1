b96749a56f481f95355e0a020975688f7d3932fb
Shubham Kamal <shubhamkamal44@gmail.com>
2024-01-18 17:41:48 +0530

    This is my first commit as lead.

---
+++ src/com/lead/Test.java
@@ -0,0 +1,14 @@
+package com.lead;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String hungery = "yes";
+        if(hungery.equalsIgnoreCase("yes"))
+        	System.out.println("Please eat somthing....");
+        else
+        	System.out.println("Please do work....");
+	}
+
+}
